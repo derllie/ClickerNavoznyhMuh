ab75e3f591d6101775a9aad46bc31b2d54f55727
Meylishh <murmeylishh@gmail.com>
2024-02-05 11:56:55 +0300

    setup
    + added DOTween
    + added new input system and created input manager

---
+++ Assets/Script/InputSystem/InputManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(PlayerInput))]
+public class InputManager : MonoBehaviour
+{
+    private bool mouseButtonPressed;
+    private static InputManager instance;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("Found more than one Input Manager in the scene.");
+        }
+        instance = this;
+    }
+
+    public static InputManager GetInstance()
+    {
+        return instance;
+    }
+
+
+    public void MouseButtonPressed(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            mouseButtonPressed = true;
+        }
+        else if (context.canceled)
+        {
+            mouseButtonPressed = false;
+        }
+    }
+
+    // for any of the below 'Get' methods, if we're getting it then we're also using it,
+    // which means we should set it to false so that it can't be used again until actually
+    // pressed again.
+    public bool GetMouseButtonPressed()
+    {
+        bool result = mouseButtonPressed;
+        mouseButtonPressed = false;
+        return result;
+    }
+
+}
